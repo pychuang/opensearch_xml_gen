2e4e4f87b21e03b1cc6b005dd0381d25d623647e
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-01-21 21:05:45 -0500

    gen_xml.py: input from/output to files

--- gen_xml.py
+++ gen_xml.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
+import argparse
 import json
 import urllib
 import urllib2
 
-#from lxml import etree
 import xml.etree.cElementTree as ET
 
+
 SOLR_URL='http://localhost:8080/solr/citeseerx/select'
+
 
 def build_xml(root, result):
     e = ET.SubElement(root, 'topic')
     e.attrib['number'] = result['site_qid']
     ET.SubElement(e, 'query').text = result['qstr']
+
 
 def query_solr(query):
     f = {
@@ -35,28 +38,48 @@ def query_solr(query):
     results = []
     response = data['response']
     numFound = response['numFound']
-    print "Found %d documents" % numFound
+    #print "Found %d documents" % numFound
     docs = response['docs']
     for doc in docs:
         if 'doi' not in doc:
             continue
         doi = doc['doi']
-        print "DOI: %s" % doi
+        #print "DOI: %s" % doi
         result = {}
         result['qstr'] = query
         result['site_qid'] = doi
         results.append(result)
-    print "Got %d documents " % len(results)
+    #print "Got %d documents " % len(results)
     return results
 
-def main():
-    results = query_solr('machine learning')
-    print results
 
-    root = ET.Element("root")
+def process_one_query(root, query):
+    results = query_solr(query)
+
     for result in results:
         build_xml(root, result)
-    print ET.tostring(root)
+
+
+def process(inputfile, outputfile):
+    root = ET.Element("root")
+
+    with open(inputfile) as ins:
+        for line in ins:
+            query = line.strip()
+            process_one_query(root, query)
+
+    tree = ET.ElementTree(root)
+    tree.write(outputfile)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Query Solr and save results as XML for TREC OpenSearch.')
+    parser.add_argument('--input', required=True, help='specify input file')
+    parser.add_argument('--output', required=True, help='specify output file')
+
+    args = parser.parse_args()
+    process(args.input, args.output)
+
 
 if __name__ == '__main__':
     main()
