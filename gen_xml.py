70a3b1a2443aa84ebe6872cb91960fc856dc1b0f
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-01-27 15:23:55 -0500

    gen_xml.py: generate runfile and docdir

--- gen_xml.py
+++ gen_xml.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+
 import argparse
+import codecs
+import hashlib
 import json
+import os
 import urllib
 import urllib2
 
@@ -10,10 +14,8 @@ import xml.etree.cElementTree as ET
 SOLR_URL='http://localhost:8080/solr/citeseerx/select'
 
 
-def build_xml(root, result):
-    e = ET.SubElement(root, 'topic')
-    e.attrib['number'] = result['site_qid']
-    ET.SubElement(e, 'query').text = result['qstr']
+def generate_site_query_id(query):
+    return hashlib.sha1(query).hexdigest()
 
 
 def query_solr(query, n):
@@ -35,51 +37,64 @@ def query_solr(query, n):
     if not header or header['status'] != 0:
         return []
 
-    results = []
     response = data['response']
-    numFound = response['numFound']
+    #numFound = response['numFound']
     #print "Found %d documents" % numFound
-    docs = response['docs']
-    for doc in docs:
+    return response['docs']
+
+
+def build_query_list_xml(root, qid, query):
+    e = ET.SubElement(root, 'topic')
+    e.attrib['number'] = qid
+    ET.SubElement(e, 'query').text = query
+
+
+def save_doclist(runfile, docdir, qid, solrdocs):
+    for doc in solrdocs:
         if 'doi' not in doc:
             continue
-        doi = doc['doi']
-        #print "DOI: %s" % doi
-        result = {}
-        result['qstr'] = query
-        result['site_qid'] = doi
-        results.append(result)
-    #print "Got %d documents " % len(results)
-    return results
+        docid = doc['doi']
+        title = doc['title']
+        content = doc['abstract']
+
+        # doclist run file
+        line = ' '.join([qid, "dontcare", docid, "dontcare", "dontcare", "dontcare"]) + "\n"
+        runfile.write(line)
+
+        # docs
+        f = codecs.open(os.path.join(docdir, docid), "w", "utf-8")
+        f.write(title + "\n")
+        f.write(content)
 
 
-def process_one_query(root, query, n):
-    results = query_solr(query, n)
+def process(inputfile, n):
+    qlist_file_name = "qlist.xml"
+    docdir = "docdir"
+    runfile_name = "doclist.runfile"
+    if not os.path.exists(docdir):
+        os.makedirs(docdir)
 
-    for result in results:
-        build_xml(root, result)
-
-
-def process(inputfile, outputfile, n):
-    root = ET.Element("root")
+    runfile = open(runfile_name, "w")
+    qlist_root = ET.Element("root")
 
     with open(inputfile) as ins:
         for line in ins:
             query = line.strip()
-            process_one_query(root, query, n)
+            solrdocs = query_solr(query, n)
+            qid = generate_site_query_id(query)
+            build_query_list_xml(qlist_root, qid, query)
+            save_doclist(runfile, docdir, qid, solrdocs)
 
-    tree = ET.ElementTree(root)
-    tree.write(outputfile)
+    ET.ElementTree(qlist_root).write(qlist_file_name)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Query Solr and save results as XML for TREC OpenSearch.')
     parser.add_argument('--input', required=True, help='specify input file')
-    parser.add_argument('--output', required=True, help='specify output file')
     parser.add_argument('--num', required=True, help='number of results for each query')
 
     args = parser.parse_args()
-    process(args.input, args.output, int(args.num))
+    process(args.input, int(args.num))
 
 
 if __name__ == '__main__':
